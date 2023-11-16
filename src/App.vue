c100c019cc02d1bd9e53b0d0b98ada879af7bc14
Ester-0 <azalea_@hotmail.com>
2023-11-16 09:02:46 +0100

    Ok

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import IdCard from './components/IdCard.vue';
+import Random from './components/Random.vue';
+
 
 
 </script>
+
+
 
 <template>
   <h1>Creación de componentes con Vue</h1>
@@ -10,15 +14,27 @@ import IdCard from './components/IdCard.vue';
     <h2>Id Card</h2>
     <IdCard last-name='Doe' first-name='John' gender='male' :height="178" :birth='new Date("1992-07-14")'
       picture="https://randomuser.me/api/portraits/men/44.jpg" />
-  </section>
-
-  <section>
-    <h2>Id Card</h2>
+  
+    
     <IdCard last-name='Dolores' first-name='Obrien' gender='female' :height="172" :birth='new Date("1993-05-11")'
       picture="https://randomuser.me/api/portraits/women/8.jpg" />
   </section>
   
+<section>
+  <h1>Random</h1>
+  <Random :min="1" :max="6"/>
+<Random :min="1" :max="100"/>
+
+<!-- <Random min="1" max="6" :numRandom="Math.floor(Math.random() * 'max') + 1"/> -->
+
+
+</section>
+
 </template>
+
+
+
+
 
 <style scoped>
 h2 {
---
+++ src/components/Random.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['min', 'max'])
+
+</script>
+
+
+
+<template>
+    <div>
+        <p>Random value between: {{ min }} and {{ max }} => {{ Math.floor(Math.random() * max -min+1) + min }} </p>
+        
+
+    </div>
+</template>
+
+<style></style>
